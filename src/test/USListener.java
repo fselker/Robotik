e68e661854515009dfcc2078b701ff2fa69a5cb3
nomissretep <nomissretep@yahoo.de>
2014-04-10 09:53:34 +0200

    pipen bei grosser entfernung

--- src/test/USListener.java
+++ src/test/USListener.java
@@ -8,7 +8,8 @@ public class USListener implements SensorPortListener{
 	@Override
 	public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
 		// TODO Auto-generated method stub
-		
+		if ( aNewValue>20&&aOldValue > aNewValue)
+			Sound.playTone (dist*10+100,300,100);
 	}
 
 
